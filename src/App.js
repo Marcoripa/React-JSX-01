a17ae7b0c358bc0dd2abb93d6839598efaf4e723
Marco Ripa <marcoripa@hotmail.it>
2022-05-24 02:55:25 +1000

    Update App.js
    Combining ex.1, 2 and 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+//esercizio numero 1
 let hello = 'hello world'
 
 function Greeting() {
@@ -9,7 +11,28 @@ function Greeting() {
   )
 }
 
-
-
-
 export default Greeting
+
+//esercizio numero 2
+
+//function GreetWithName(name) {
+//  name = 'Marco'
+//  return (
+//    <h1>Hello, {name}</h1>
+//  )
+//}
+
+
+//export default GreetWithName;
+
+//esercizio numero 3
+
+//function Sum(a, b) {
+//    a = 2
+//    b = 3
+//    return (
+//      <h2>{a + b}</h2>
+//    )
+//    }
+    
+//export default Sum;
